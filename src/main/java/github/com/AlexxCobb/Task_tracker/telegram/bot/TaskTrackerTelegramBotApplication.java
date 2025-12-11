07c9c55878df369600c3a309929812fc3e29795e
Александр <zpeoplle@yandex.ru>
2025-12-11 12:44:41 +0300

    Initial commit

---
+++ src/main/java/github/com/AlexxCobb/Task_tracker/telegram/bot/TaskTrackerTelegramBotApplication.java
@@ -0,0 +1,13 @@
+package github.com.AlexxCobb.Task_tracker.telegram.bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskTrackerTelegramBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskTrackerTelegramBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/github/com/AlexxCobb/Task_tracker/telegram/bot/TaskTrackerTelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package github.com.AlexxCobb.Task_tracker.telegram.bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskTrackerTelegramBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
